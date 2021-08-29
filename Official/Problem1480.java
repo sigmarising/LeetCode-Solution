e2288c7d198ea012fdb5249307845e960fc96fa7
Yun Zhang <sigmarising@hotmail.com>
2021-08-29 11:37:21 +0800

    [ADD]: 2021.8.29 Add Solution for Problem 1480 1588.

---
+++ Official/Problem1480.java
@@ -0,0 +1,15 @@
+package Official;
+
+public class Problem1480 {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            result[i] = sum;
+        }
+
+        return result;
+    }
+}
---
+++ Official/Problem1588.java
@@ -0,0 +1,21 @@
+package Official;
+
+public class Problem1588 {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int left = i + 1;
+            int right = arr.length - i;
+
+            int left_even = (left + 1) / 2;
+            int right_even = (right + 1) / 2;
+
+            int left_odd = left / 2;
+            int right_odd = right / 2;
+
+            result += (left_even * right_even + left_odd * right_odd) * arr[i];
+        }
+
+        return result;
+    }
+}
